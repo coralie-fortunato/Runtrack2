c00aff2e56c08cbb93dce125f3c780ca2f85bbd5
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-05-04 10:43:32 +0200

    Réalisation job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+    $str = "Hello LaPlateforme";
+    echo $str; 
+
+    $str2 ="Vive";
+    $str3 = " ! ";
+    $str = "LaPlateforme";
+    echo  "\n" . $str2 . ' ' . $str . $str3;
+
+    $val = 6;
+    echo  "\n". $val;
+    $val= $val + 4;
+    echo "\n". $val;
+
+    $myBool = true;
+    echo "\n". $myBool;
+
+    $myBool = false;
+    echo "\n". $myBool;
+
+
+
+?>
