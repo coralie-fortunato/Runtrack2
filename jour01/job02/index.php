ee19433ac929619812138b03b2a085006ac91a9d
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-05-04 11:52:52 +0200

    Réalisation job 2

---
+++ jour01/job02/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo "Hello LaPlateforme!";
+    //Je commente
+
+    $job02 = "Hello LaPlateforme!";
+    echo "\n" . $job02;
+    /*Un autre commentaire*/
+
+
+    # On peut aussi faire :
+    /* $job02 = "Hello LaPlateforme! ";
+     echo str_repeat($job02, 2); */
+
+
+
+?>
