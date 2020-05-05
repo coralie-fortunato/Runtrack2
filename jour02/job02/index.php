f50c47d597642507ad9ccd41dc8499228cb91785
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-05-05 13:46:41 +0200

    Réalisation job02

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -1,5 +1,33 @@
 <?php
-
+     $min = 0;
+     $max = 1337;
+ 
+     for ( $number = $min; $number <= $max; $number++){
+         if( $number <= 25){
+            echo $number . "<br/>";
+         }
+     }
+     for ( $number = $min; $number <= $max; $number++){
+         if( $number >= 27 && $number <= 36){
+            echo $number . "<br/>";
+         }
+     }
+     for ( $number = $min; $number <= $max; $number++){
+         if( $number >= 38 && $number <= 87){
+            echo $number . "<br/>";
+         }
+     }
+     for ( $number = $min; $number <= $max; $number++){
+         if( $number >= 89 && $number <= 1110){
+            echo $number . "<br/>";
+         }
+     }
+     for ( $number = $min; $number <= $max; $number++){
+         if( $number >= 1112 && $number <= $max){
+            echo $number . "<br/>";
+         }
+     }
+    
 
 
 ?>
