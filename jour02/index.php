0b8978f87b26527ddde6de4ad7dd5c02d77345c9
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-05-05 13:19:32 +0200

    Réalisation job 01

---
+++ jour02/index.php
@@ -0,0 +1,25 @@
+<?php
+    $min = 0;
+    $max = 1337;
+
+    for ( $number = $min; $number <= $max; $number++){
+		
+		if ($number <=41){
+		echo $number . "<br/>";}	
+		
+    }
+	if($number = 42){
+			echo "<b><u> $number <br/> </u></b>";}
+			
+	for ( $number = $min; $number <= $max; $number++){
+	if ($number >=43){
+			echo $number . "<br/>";
+		}
+	}
+			
+	
+
+     
+
+?>
+
