18b6f4a961d433eb186d6b90c729de35d6440ff4
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-05-07 10:25:27 +0200

    Réalisation job01

---
+++ jour04/job01/index.php
@@ -0,0 +1,35 @@
+<?php
+$var= 0;
+foreach($_GET as $k => $values){
+    $var++;
+}
+echo "Nombre d'argument:  $var";
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job01</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+    <label for="name">Votre nom</label>
+    <input type="text" name="name" id="name" required>
+    <br>
+
+    <label for="prenom">Votre prénom</label>
+    <input type="text" name="prenom" id="prenom" required>
+    <br>
+    <label for="mail">Votre Email:</label>
+    <input type="email" name="mail" id="mail" required>
+    <br>
+    <button type="submit">Entrez</button>
+
+
+
+    </form>
+    
+</body>
+</html>
