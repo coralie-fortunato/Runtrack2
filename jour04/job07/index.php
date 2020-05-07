553327324650436e2ae38c23d65345ab4ae44b96
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-05-07 21:59:52 +0200

    Exercice 7, pas reussi

---
+++ jour04/job07/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 07</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+        <label for="largeur">Largeur</label>
+        <input type="text" name= "largeur" id="largeur">
+        <br>
+        <label for="hauteur">Hauteur</label>
+        <input type="text" name= "hauteur" id="hauteur">
+        <br>
+        <button type="submit" name ="valider">Valider</button>
+    </form>
+</body>
+</html>
