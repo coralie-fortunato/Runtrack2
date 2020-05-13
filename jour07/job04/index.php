a6929f446112fe6125e2a9b380ac16b1b2aff047
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-05-13 19:18:59 +0200

    Réalisation job 04

---
+++ jour07/job04/index.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_POST["deconnecter"])){
+    $var="";
+    setcookie("prenom", $var);
+    $_COOKIE=$var;
+
+}
+if(isset($_POST["connexion"]) && isset($_POST["prenom"] )){
+    setcookie("prenom", $_POST["prenom"]);
+    $_COOKIE["prenom"] = $_POST["prenom"];
+}
+if(!isset($_COOKIE["prenom"]))
+{
+     echo '<form action="test.php" method="post">
+     <input type=text name="prenom">
+     <button type="submit" name="connexion">Se connecter</button>
+
+
+    </form>';
+ } 
+if(isset($_COOKIE["prenom"]) ){
+    echo "Bonjour" . " "  . $_COOKIE["prenom"] . "<br />";
+    echo '<form action="test.php" method="post">
+    <button type="submit" name="deconnecter">Se déconnecter</button>
+    </form>';
+}
+
+
+?>
