875163c60742f4aedabb9996702a5018d46e2cf2
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-05-13 10:34:30 +0200

    Réalisation job01

---
+++ jour07/job01/index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$nbvisites = 1 ;
+function compteur(){
+    if(file_exists('page-test.php')){
+        $nbvisites = (int)file_get_contents('page-test.php');
+        $nbvisites++;
+       
+    }
+    file_put_contents('page-test.php', $nbvisites);
+}
+
+function nombre_vues(){
+ if(file_exists("page-test.php")){
+
+ }
+
+  
+  return file_get_contents('page-test.php');
+}
+compteur();
+echo "La page a été vu " . nombre_vues() ." fois" . "<br />";
+
+?>
