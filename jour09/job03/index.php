52689f4bb67b85063fa2f09c43b144bb6d4afaf1
coralie-fortunato <62133628+coralie-fortunato@users.noreply.github.com>
2020-05-15 12:07:42 +0200

    Réalisation job03

---
+++ jour09/job03/index.php
@@ -0,0 +1,35 @@
+<?php
+$db= mysqli_connect("localhost","root","","jour08");
+$requete = "SELECT  `prenom`, `nom`, `naissance`
+            FROM `etudiants` 
+            WHERE sexe= 'femme'";
+$requete2 = "SELECT COLUMN_NAME
+            FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_NAME='etudiants'
+            AND column_name='nom' OR column_name='prenom'
+            OR column_name='naissance'";
+$query= mysqli_query($db,$requete);
+$query2= mysqli_query($db,$requete2);
+$data = mysqli_fetch_all($query);
+$column_name = mysqli_fetch_all($query2); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <?php
+           for($i=0; $i<=count($column_name); $i++ ){
+               echo "<th>" . $column_name[$i] . "</th>";
+           }
+           ?>
+        </thead>
+    </table>
+    
+</body>
+</html>
